77efb0c50e09fc5410cccef91d080d4addf7e7db
Jerry Gu <jerrysonggu@gmail.com>
2026-04-09 23:16:59 -0400

    5.2 creating a raytraced image

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include "color.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& ray) {
+    vec3 C_minus_Q = center - ray.origin();
+    auto a = dot(ray.direction(), ray.direction());
+    auto b = -2.0 * dot(ray.direction(), C_minus_Q);
+    auto c = dot(C_minus_Q, C_minus_Q) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
+
 color ray_color(const ray& ray) {
+    if (hit_sphere(point3(0, 0, -1), .5, ray))
+        return color(0, 1, 0);
+
     vec3 unit_direction = unit_vector(ray.direction());
     auto a = .5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
