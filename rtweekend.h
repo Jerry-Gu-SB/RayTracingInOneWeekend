da906aa1ccf48063ca5bc4ecc3fdcda11ff5c803
Jerry Gu <jerrysonggu@gmail.com>
2026-06-01 09:10:53 -0400

    8.1. Some Random Number Utilities

--- rtweekend.h
+++ rtweekend.h
@@ -6,6 +6,7 @@
 #define RAYTRACINGINONEWEEKEND_RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -25,6 +26,15 @@ const  double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    // Returns random number [0, 1). Note not including 1 is important later apparently
+    return std::rand() / (RAND_MAX + 1.0); // RAND_MAX = 2147483647.
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
