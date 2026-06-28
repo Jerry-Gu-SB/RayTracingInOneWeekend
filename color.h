a84df94ca36390656d18309c9661be94aa72d2e0
Jerry Gu <jerrysonggu@gmail.com>
2026-06-27 21:56:21 -0400

    9.5 Using Gamma Correction for Accurate Color Intensity

--- color.h
+++ color.h
@@ -9,10 +9,25 @@
 
 using color = vec3;
 
+// So our image viewer is kind of dark because we're feeding it linear images, but it's then displaying those images in
+// a gamma space. So we're correcting it by transforming it to gamma n, where the formula is (1/gamma)^n. Here it's
+// gamma 2 = (1 / gamma)^2 = sqrt(gamma). Images do this by default because our eyes don't perceive light linearly.
+// Pretty nuts stuff.
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Transforms colors from linear --> gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
