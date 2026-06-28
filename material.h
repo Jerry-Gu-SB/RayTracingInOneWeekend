c6a44392491c80cea58da6b0634e4d2ba442826c
Jerry Gu <jerrysonggu@gmail.com>
2026-06-27 22:12:25 -0400

    10.1 An Abstract Class for Materials

---
+++ material.h
@@ -0,0 +1,20 @@
+﻿//
+// Created by jerry on 6/27/2026.
+//
+
+#ifndef RAYTRACINGINONEWEEKEND_MATERIAL_H
+#define RAYTRACINGINONEWEEKEND_MATERIAL_H
+
+#include "hittable.h"
+
+class material {
+public:
+    virtual ~material() = default;
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const {
+        return false;
+    }
+};
+#endif // RAYTRACINGINONEWEEKEND_MATERIAL_H
