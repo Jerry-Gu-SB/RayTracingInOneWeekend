8d07882ef8ebf97f17d5a637394085652f741da0
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2026-09-08 03:35:41 -0400

    12.2 positioning and orienting the camera

--- camera.h
+++ camera.h
@@ -19,6 +19,9 @@ public:
 
     double vfov = 90; // vertical view angle or field of view
 
+    point3 lookfrom = point3(0, 0, 0); // point where the camera is looking from
+    point3 lookat = point3(0, 0, -1); // the point that we're looking at
+    vec3 vup = vec3(0, 1, 0); // camera-relative "up" direction. note that this can be whatever we want
 
     void render(const hittable& world) {
         initialize();
@@ -51,6 +54,8 @@ private:
     vec3 pixel_delta_u; // Offset to pixel to the right
     vec3 pixel_delta_v; // Offset to pixel below
 
+    vec3 u, v, w;       // These are the 3 vectors that determine our camera roll and angle
+                        // u = camera right, v = camera up, w = camera opposite view direction (right hand rule), and camera center is at the origin
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
@@ -58,25 +63,30 @@ private:
 
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
-        camera_center = point3(0, 0, 0);  // a.k.a. the "eye point"
+        camera_center = lookfrom;
 
         // determien viewport dimensions
-        const auto focal_length = 1.0;  // distance between camera center, and the 2d plane of the viewport
+        const auto focal_length = (lookfrom - lookat).length();  // distance between camera center, and the 2d plane of the viewport
         auto theta = degrees_to_radians(vfov);
         auto h = std::tan(theta / 2.0);
         auto viewport_height = 2 * h * focal_length;
         const auto viewport_width = viewport_height * (double(image_width)/image_height);
 
+        // u, v, w unit vectors
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
         // Calculate vectors across the horizontal and down the vertical viewport edges. The dimensions of our viewport as a vector
-        const auto viewport_u = vec3(viewport_width, 0, 0);
-        const auto viewport_v = vec3(0, -viewport_height, 0);
+        const auto viewport_u = viewport_width * u; // vector going across the viewport edge of the rectangle
+        const auto viewport_v = viewport_height * -v; // vector down viewport vertical edge of the rectangle. i think it's negative because we start top left and go down
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel. So how far apart each pixel is basically.
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate location of upper left pixel. This is to help us with convention and to help us start from top left
-        const auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        const auto viewport_upper_left = camera_center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + .5 * (pixel_delta_u + pixel_delta_v);
 
     }
